2f7f773d0b37217b157e742b07da1d2a14215ab9
ssgh <yshyeon1028@naver.com>
2023-06-26 13:07:02 +0900

    구현되지 않은 userController, userService 생성

---
+++ src/main/java/com/project/sns/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.project.sns.controller;
+
+import com.project.sns.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    // TODO : implement
+    @PostMapping
+    public void join() {
+        userService.join();
+    }
+}
---
+++ src/main/java/com/project/sns/service/UserService.java
@@ -0,0 +1,13 @@
+package com.project.sns.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Service
+public class UserService {
+
+    @PostMapping
+    public void join() {
+
+    }
+}
