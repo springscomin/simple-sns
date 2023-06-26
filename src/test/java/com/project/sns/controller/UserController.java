cde0e00ecd49ab92ba47b38109e3b117717e9bb6
ssgh <yshyeon1028@naver.com>
2023-06-26 13:41:24 +0900

    userController test 작성 - 회원가입/로그인

--- src/test/java/com/project/sns/controller/UserController.java
+++ src/test/java/com/project/sns/controller/UserController.java
@@ -2,13 +2,19 @@ package com.project.sns.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.sns.controller.request.UserJoinRequest;
+import com.project.sns.exception.SnsApplicationException;
+import com.project.sns.model.User;
+import com.project.sns.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,10 +29,16 @@ public class UserController {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockBean
+    private UserService userService;
+
     @Test
     public void 회원가입() throws Exception {
         String userName = "userName";
         String password = "password";
+
+
+        when(userService.join()).thenReturn(mock(User.class));
 
         mockMvc.perform(post("/api/v1/users/join")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -40,10 +52,54 @@ public class UserController {
         String userName = "userName";
         String password = "password";
 
+        when(userService.join()).thenThrow(new SnsApplicationException());
+
         mockMvc.perform(post("/api/v1/users/join")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password)))
                 ).andDo(print())
                 .andExpect(status().isConflict());
+    }
+
+    @Test
+    public void 로그인() throws Exception {
+        String userName = "userName";
+        String password = "password";
+
+        when(userService.login()).thenReturn("secret_token");
+
+        mockMvc.perform(post("/api/v1/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password)))
+                ).andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void 로그인시_회원가입이_안된_userName을_입력할경우_에러반환() throws Exception {
+        String userName = "userName";
+        String password = "password";
+
+        when(userService.login()).thenThrow(new SnsApplicationException());
+
+        mockMvc.perform(post("/api/v1/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password)))
+                ).andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void 로그인시_틀린_password를_입력할경우_에러반환() throws Exception {
+        String userName = "userName";
+        String password = "password";
+
+        when(userService.login()).thenThrow(new SnsApplicationException());
+
+        mockMvc.perform(post("/api/v1/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password)))
+                ).andDo(print())
+                .andExpect(status().isUnauthorized());
     }
 }
